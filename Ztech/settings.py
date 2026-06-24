ec8b7928a83edd2f102711125ec29ed91b1760a1
rokiaremache <rokia.remache@gmail.com>
2026-06-24 16:58:53 +0100

    add cloudinary storage

--- Ztech/settings.py
+++ Ztech/settings.py
@@ -151,14 +151,9 @@ LOGGING = {
 INSTALLED_APPS += ['cloudinary', 'cloudinary_storage']
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'dwlah6mw4',
-    'API_KEY': '844844414645432',
-    'API_SECRET': 'PnhwIilles3YrxY-G80UruszUL8',
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', 'dwlah6mw4'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', '844844414645432'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', 'PnhwIilles3YrxY-G80UruszUL8'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
